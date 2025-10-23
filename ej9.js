7efd3aaf2bd3066dfdf3cc2e42a064f5e58f49cf
Adrian25876 <adribermudez6587@gmail.com>
2025-10-23 12:56:03 +0200

    Add prompt for bachiller title check

--- ej9.js
+++ ej9.js
@@ -1,4 +1,5 @@
 //Realizar algoritmo que nos diga si una persona puede acceder a cursar un ciclo formativo de grado superior o no. Para acceder a un grado superior, si se tiene un título de bachiller, en caso de no tenerlo, se puede acceder si hemos superado una prueba de acceso.
+let prompt = require(`prompt-sync`)();
 let tieneBachiller = prompt("¿Tienes título de bachiller? (si/no)");
 
 if (tieneBachiller === "si") {
